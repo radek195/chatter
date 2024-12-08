c2518e8cfe414d29a5dd270a2377868029fb4bde
radek195 <radoslaw.parol95@gmail.com>
2024-12-08 14:17:28 +0100

    init

---
+++ src/test/java/pl/radek/chatter/ChatterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.radek.chatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
