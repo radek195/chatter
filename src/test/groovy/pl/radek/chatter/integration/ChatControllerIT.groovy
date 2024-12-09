de1a07882ada321e554d332e7285d208ac678f92
radek195 <radoslaw.parol95@gmail.com>
2024-12-09 19:14:39 +0100

    set up spock and groovy

---
+++ src/test/groovy/pl/radek/chatter/integration/ChatControllerIT.groovy
@@ -0,0 +1,11 @@
+package pl.radek.chatter.integration
+
+import spock.lang.Specification
+
+class ChatControllerIT extends Specification {
+
+    def "should work!"() {
+        expect:
+            1 == 1
+    }
+}
