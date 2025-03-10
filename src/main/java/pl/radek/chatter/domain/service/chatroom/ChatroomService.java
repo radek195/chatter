1721fecb009f5336af64729a7eab7f7234f57da7
radek195 <radoslaw.parol95@gmail.com>
2025-03-10 21:03:44 +0100

    add method to return chatroomId that matches user preference

--- src/main/java/pl/radek/chatter/domain/service/chatroom/ChatroomService.java
+++ src/main/java/pl/radek/chatter/domain/service/chatroom/ChatroomService.java
@@ -2,7 +2,12 @@ package pl.radek.chatter.domain.service.chatroom;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.radek.chatter.domain.service.user.matching.UserMatchingService;
+import pl.radek.chatter.infrastructure.exceptions.UserNotFoundException;
+import pl.radek.chatter.infrastructure.repository.user.UserEntity;
+import pl.radek.chatter.infrastructure.repository.user.UserRepository;
 
+import java.util.List;
 import java.util.Queue;
 import java.util.UUID;
 
@@ -10,10 +15,14 @@ public interface ChatroomService {
 
     UUID getChatroom();
 
+    UUID getMatchingChatroom(Long userId);
+
     @Service
     @RequiredArgsConstructor
     class Impl implements ChatroomService {
 
+        private final UserRepository userRepository;
+        private final UserMatchingService userMatchingService;
         private final Queue<UUID> chatrooms;
 
         public UUID getChatroom() {
@@ -23,6 +32,26 @@ public interface ChatroomService {
                 chatrooms.offer(newChatroomId);
                 return newChatroomId;
             }
+            return chatroomId;
+        }
+
+        //TODO fix race condition in that method
+        @Override
+        public UUID getMatchingChatroom(Long userId) {
+            UserEntity requestingUser = userRepository.findById(userId)
+                    .orElseThrow(() -> new UserNotFoundException("Could not find user with id: " + userId));
+
+            List<UserEntity> users = userRepository.findAllWithUniqueChatroomIdExcluding(userId);
+            List<UserEntity> matchingUserList = userMatchingService.findMatchingUsersByPreference(requestingUser, users);
+
+            UUID chatroomId;
+            if (matchingUserList.isEmpty()) {
+                chatroomId = UUID.randomUUID();
+            } else {
+                chatroomId = matchingUserList.get(0).getChatroomId();
+            }
+
+            requestingUser.setChatroomId(chatroomId);
             return chatroomId;
         }
     }
--- src/test/groovy/pl/radek/chatter/unit/ChatroomServiceUT.groovy
+++ src/test/groovy/pl/radek/chatter/unit/ChatroomServiceUT.groovy
@@ -1,18 +1,28 @@
 package pl.radek.chatter.unit
 
 import pl.radek.chatter.domain.service.chatroom.ChatroomService
+import pl.radek.chatter.domain.service.user.matching.UserMatchingService
+import pl.radek.chatter.infrastructure.exceptions.UserNotFoundException
+import pl.radek.chatter.infrastructure.repository.user.UserEntity
+import pl.radek.chatter.infrastructure.repository.user.UserRepository
 import spock.lang.Specification
+import spock.lang.Subject
 
 class ChatroomServiceUT extends Specification {
     
+    UserRepository userRepository = Mock()
+    UserMatchingService userMatchingService = Mock()
+    
+    @Subject
     ChatroomService chatroomService
+    
     
     def 'Should return existing chatroom'() {
         given: 'Chatroom in the queue exists'
             UUID uuid = UUID.randomUUID()
             Queue<UUID> queue = new LinkedList<>()
             queue.offer(uuid)
-            chatroomService = new ChatroomService.Impl(queue)
+            chatroomService = new ChatroomService.Impl(userRepository, userMatchingService, queue)
         
         when: 'Retrieving chatroom'
             UUID retrievedUuid = chatroomService.getChatroom()
@@ -25,7 +35,7 @@ class ChatroomServiceUT extends Specification {
     def 'should not offer new chatroom when queue is not empty'() {
         given: 'Poll for queue is mocked to return UUID'
             def chatroomsMock = Mock(LinkedList<UUID>)
-            chatroomService = new ChatroomService.Impl(chatroomsMock)
+            chatroomService = new ChatroomService.Impl(userRepository, userMatchingService, chatroomsMock)
             1 * chatroomsMock.poll() >> UUID.randomUUID()
         
         when: 'Retrieving chatroom'
@@ -37,7 +47,7 @@ class ChatroomServiceUT extends Specification {
     
     def 'Should return new chatroom'() {
         given: 'Chatroom in the queue does not exist'
-            chatroomService = new ChatroomService.Impl(new LinkedList<>())
+            chatroomService = new ChatroomService.Impl(userRepository, userMatchingService, new LinkedList<>())
         
         when: 'Retrieving new chatroom'
             UUID retrievedUuid = chatroomService.getChatroom()
@@ -50,7 +60,7 @@ class ChatroomServiceUT extends Specification {
     def 'should offer new chatroom when queue is empty'() {
         given: 'Poll for queue is mocked to return null'
             def chatroomsMock = Mock(LinkedList<UUID>)
-            chatroomService = new ChatroomService.Impl(chatroomsMock)
+            chatroomService = new ChatroomService.Impl(userRepository, userMatchingService, chatroomsMock)
             1 * chatroomsMock.poll() >> null
         
         when: 'Retrieving chatroom'
@@ -59,5 +69,57 @@ class ChatroomServiceUT extends Specification {
         then: 'Offer should not be called'
             1 * chatroomsMock.offer(_)
     }
+    
+    def setup() {
+        chatroomService = new ChatroomService.Impl(userRepository, userMatchingService, Mock(Queue<UUID>))
+    }
+    
+    def 'should return existing chatroomId when matching user is found'() {
+        given:
+            UserEntity requestingUser = Mock()
+            UserEntity matchingUser = Spy(new UserEntity())
+            UUID existingChatroomId = UUID.randomUUID()
+            matchingUser.setChatroomId(existingChatroomId)
+            
+            userRepository.findById(_) >> Optional.of(requestingUser)
+            userRepository.findAllWithUniqueChatroomIdExcluding(_) >> List.of(matchingUser)
+            userMatchingService.findMatchingUsersByPreference(_, _) >> List.of(matchingUser)
+        
+        when:
+            UUID result = chatroomService.getMatchingChatroom(1)
+        
+        then:
+            1 * matchingUser.getChatroomId()
+            1 * requestingUser.setChatroomId(existingChatroomId)
+            result == existingChatroomId
+    }
+    
+    def 'should return new chatroomId when matching user is not'() {
+        given:
+            UserEntity requestingUser = Mock()
+        
+            userRepository.findById(_) >> Optional.of(requestingUser)
+            userRepository.findAllWithUniqueChatroomIdExcluding(_) >> List.of()
+            userMatchingService.findMatchingUsersByPreference(_, _) >> List.of()
+        
+        when:
+            UUID result = chatroomService.getMatchingChatroom(1)
+        
+        then:
+            1 * requestingUser.setChatroomId(_)
+            result !== null
+    }
+    
+    def 'should throw UserNotFoundException when requesting user id cannot be found'() {
+        given:
+            userRepository.findById(2) >> Optional.empty()
+        
+        when:
+            chatroomService.getMatchingChatroom(2)
+        
+        then:
+            thrown(UserNotFoundException)
+    }
+    
     
 }
